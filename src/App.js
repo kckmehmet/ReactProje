137bae58616cfdd83bc4486f6566cab2437b54ed
mehmet <mehmetkucukk_@outlook.com>
2025-02-02 22:18:53 +0300

    yazi yenilendi

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-            Mehmet'in sitesine Hoş geldiniz
+            Git & Github Kursuna Hoşgeldiniz!! w/Mehmet
         </p>
         <a
           className="App-link"
